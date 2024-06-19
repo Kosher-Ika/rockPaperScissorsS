a7c29f266113ff6ba1f192aabfd6b0e02f31a25e
Kosher-Ika <irakligoshteliani9@gmail.com>
2024-06-19 09:58:02 +0400

    did many things

--- script.js
+++ script.js
@@ -15,4 +15,40 @@ function disableButtons() {
     })
 }
 
-// playround
+// playRound
+function playRound(playerSection) {
+    let computerSection = computerChoice()
+    let show = ""
+
+    if(playerSection == "rock" && computerSection == "scissors" ||
+        playerSection == "paper" && computerSection == "rock" ||
+        playerSection == "scissors" && computerSection == "paper") {
+     playerScore+=1
+    show = " Good Luck! " + playerSection +" beats " + computerSection + "<br> player Score - " + playerScore + "<br> computer score - " + computerScore
+    if(playerScore==5) {
+        show+= "<br> game Over! you won!"
+        disableButtons()
+    }
+}
+
+else if (playerSection == computerSection) {
+    show = " its a tie! you both chose " + computerSection+ "<br> player Score - " + playerScore + "<br> computer score - " + computerScore
+}
+ else {
+    computerScore+=1
+    show = " Bad Luck! " + computerSection +" beats " + playerSection + "<br> player Score - " + playerScore + "<br> computer score - " + computerScore
+    if(computerScore==5) {
+        show+= "<br> game Over! you Lost!"
+        disableButtons()
+    }
+
+}
+
+document.querySelector(".show").innerHTML = show
+}
+
+buttons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        playRound(btn.value)
+    })
+})
